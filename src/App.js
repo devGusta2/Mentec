b72461fb78a11f7779e28fe8957b209cb1e8be48
devGusta2 <gustavo.souza2979@gmail.com>
2024-08-23 23:15:30 -0300

    First section

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className='main'>
+    	  <section>
+          <div className='up-box'>
+            <h1>Mentec</h1>
+          </div>
+          <div className='center-box'>
+            
+            <div className='info-mentec'>
+            <h1>Mentorias de<br/>tecnlogia</h1>
+              <p>Transforme seus objetivos em realidade com a orientação 
+                certa. Junte-se a nós e comece sua jornada para o sucesso!</p>
+              <a href='#'className='btn-find'>
+                <h3>Encontrar mentorias</h3>
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
