4f01664e86de5a688bd43a0c0e87ef5ebde1afca
devGusta2 <gustavo.souza2979@gmail.com>
2024-08-24 20:43:39 -0300

    Navgation bar

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ function App() {
       <main className='main'>
     	  <section>
           <div className='up-box'>
-            <h1>Mentec</h1>
+            <div className='top-elements'>
+              <nav>
+                
+              </nav>
+            </div>
+            <div className='top-elements'>
+              <h1>Mentec</h1>
+            </div>
           </div>
           <div className='center-box'>
-            
             <div className='info-mentec'>
-            <h1>Mentorias de<br/>tecnlogia</h1>
+            <h1>Mentorias de<br/>tecnologia</h1>
               <p>Transforme seus objetivos em realidade com a orientação 
                 certa. Junte-se a nós e comece sua jornada para o sucesso!</p>
               <a href='#'className='btn-find'>
@@ -21,6 +27,7 @@ function App() {
             </div>
           </div>
         </section>
+ 
       </main>
     </div>
   );
