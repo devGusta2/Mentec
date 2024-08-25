c0e5f715b059b0b0582f29fd4094c5c2c0f01dbd
devGusta2 <gustavo.souza2979@gmail.com>
2024-08-25 14:52:23 -0300

    Login page

--- src/Pages/login/index.js
+++ src/Pages/login/index.js
@@ -1,11 +1,51 @@
-
-
-
-
+import styles from './Login.module.css';
+import book from '../../assets/img/Arts/books.png';
+import msIco from '../../assets/img/Arts/microsft.png';
 function Login(){
     return(
-        <div className="login">
+        <div className={styles.main}>
+           <div className={styles.cadBox}>
+                <div className={styles.logoBox}>
+                    <h1  className={styles.title}>Mentec</h1>
+                    <h3  className={styles.sBtitle}>Conectando mentes, moldando futuros</h3>
+                </div>
+                <img 
+                draggable="false"
+                src={book} className={styles.imgBook}/>
+                <button className={styles.btnCad}>
+                    <h1>Cadastrar</h1>
+                </button>
+           </div>
+           <div className={styles.formBox}>
             
+                <div className={styles.form}>
+                    
+                    <form className={styles.formLogin}>
+                        <div className={styles.titleBox}>
+                            <h3>Entre na sua conta</h3>
+                        </div>
+                        <input 
+                        placeholder='E-mail'
+                        className={styles.inptForm}/>
+                        <input 
+                        placeholder='Senha'
+                        className={styles.inptForm}/>
+
+                        <button className={styles.btn}>
+                            <h3>Entrar</h3>
+                        </button>
+                        <div className={styles.or}>
+                            <hr className={styles.line}/>
+                            <h3>ou</h3>
+                            <hr className={styles.line}/>
+                        </div>
+                        <button className={styles.btnms}>
+                            <img src={msIco} style={{height:'30px'}}/>
+                            <h3>Continue com a microsoft</h3>
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
