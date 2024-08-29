e3e2fb3bc392d52a6a1b3b13c6f23f727075cf0b
devGusta2 <gustavo.souza2979@gmail.com>
2024-08-29 15:00:31 -0300

    Signup Screen

--- src/Pages/Signup/index.js
+++ src/Pages/Signup/index.js
@@ -1,7 +1,11 @@
+
+import {Link} from 'react-router-dom';
+
+import styles from 'Signup.modules.css';
 function Signup(){
     return(
-        <div className="">
-            <h1>Cadastro</h1>
+        <div className={styles.container}>
+            
         </div>
     );
 }
