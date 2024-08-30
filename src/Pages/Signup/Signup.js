69ef3a5160aa66589bc61ae85d9dfc258abc9a2c
devGusta2 <gustavo.souza2979@gmail.com>
2024-08-29 21:54:06 -0300

    Signup page

---
+++ src/Pages/Signup/Signup.js
@@ -0,0 +1,13 @@
+import styles from './Signup.module.css';
+import book from '../../assets/img/Arts/books.png';
+import msIco from '../../assets/img/Arts/microsft.png';
+import { Link } from 'react-router-dom';
+function Signup(){
+    return(
+        <div className={styles.main}>
+          
+        </div>
+    );
+}
+
+export default Signup;
