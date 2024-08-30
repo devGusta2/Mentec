15d46da905e014031080e8aaed05fca160521573
devGusta2 <gustavo.souza2979@gmail.com>
2024-08-29 22:10:06 -0300

    Signup page

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -5,7 +5,20 @@ import { Link } from 'react-router-dom';
 function Signup(){
     return(
         <div className={styles.main}>
-          
+          <div className={styles.formBox}>
+            <form className={styles.form}>
+                <h2>Crie sua conta!</h2>
+                <input placeholder="Nome:"className={styles.inpt}></input>
+                <input placeholder="E-mail:"className={styles.inpt}></input>
+                <input placeholder="Número de matricula:"className={styles.inpt}></input>
+                <input placeholder="Senha:" className={styles.inpt}></input>
+                <input placeholder="Repita sua senha:" className={styles.inpt}></input>
+                <button className={styles.btn}>Cria conta</button>
+            </form>
+          </div>
+          <div className={styles.loginBox}>
+
+          </div>
         </div>
     );
 }
