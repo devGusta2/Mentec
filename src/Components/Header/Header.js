5902ca2b7c689ea33d4138d256c31d70cda2f01b
devGusta2 <gustavo.souza2979@gmail.com>
2024-09-04 23:46:15 -0300

    Header js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,18 +5,24 @@ function Header(){
     return(
         <header className={styles.header}>
             <div className={styles.logoBox}>
-
+                <h1>Mentec</h1>
             </div>
             <div className={styles.navSearchBox}>
+                <div className={styles.searchBarBox}>
+                    <input 
+                    placeholder={"Pesquisar"}
+                    className={styles.searchBar}></input>
+                </div>
                 <nav className={styles.navBar}>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                    <ul className={styles.ul}>
+                        <li>Início</li>
+                        <li>Mentorias</li>
+                        <li>Contato</li>
+                        <li>Ajuda</li>
                     </ul>
                 </nav>
             </div>
+            <div className={styles.configBox}></div>
         </header>
     );
 };
