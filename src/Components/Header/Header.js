3bb68b3ddd9e800978820de6fc6a62bd9b20c839
devGusta2 <gustavo.souza2979@gmail.com>
2024-09-04 23:22:49 -0300

    Header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,19 @@ import styles from './Header.module.css';
 function Header(){
     return(
         <header className={styles.header}>
-            
+            <div className={styles.logoBox}>
+
+            </div>
+            <div className={styles.navSearchBox}>
+                <nav className={styles.navBar}>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 };
