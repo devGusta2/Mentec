ba45acd078598f29816fbd3024f4e0fbcbbad8ad
devGusta2 <gustavo.souza2979@gmail.com>
2024-09-05 19:57:56 -0300

    Home page

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,16 @@
+import styles from './Home.module.css';
+
+
+
+
+
+
+function Home(){
+    return(
+        <div className={styles.Home}>
+            
+        </div>
+    );
+}
+
+export default Home;
