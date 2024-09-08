724c1f47bfc90c7da719de2e059a7f951ee15355
devGusta2 <gustavo.souza2979@gmail.com>
2024-09-08 11:57:15 -0300

    Menu nav

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -2,7 +2,7 @@ import styles from './Menu.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook } from '@fortawesome/free-solid-svg-icons';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
-import { faComments } from '@fortawesome/free-solid-svg-icons';
+import { faDoorOpen } from '@fortawesome/free-solid-svg-icons';
 import { faComment } from '@fortawesome/free-solid-svg-icons';
 function Menu(){
     return(
@@ -13,13 +13,13 @@ function Menu(){
             <div className={styles.optBox}>
                 <div className={styles.navOpt}>
                     <ul>
-                        <li> <FontAwesomeIcon icon={faBook} size='2x' color='white'/>Material de apoio</li>
-                        <li> <FontAwesomeIcon icon={faGear} size='2x' color='white'/>Preferências</li>
-                        <li> <FontAwesomeIcon icon={faComments} size='2x' color='white'/>Feedback e avaliações</li>
-                        <li> <FontAwesomeIcon icon={faComment} size='2x' color='white'/>Chat</li>
+                        <li> <FontAwesomeIcon icon={faBook} size='1x'className={styles.icon} color='white'/>Material de apoio</li>
+                        <li> <FontAwesomeIcon icon={faGear} size='1x' className={styles.icon}  color='white'/>Preferências</li>
+                        <li> <FontAwesomeIcon icon={faComment} size='1x' className={styles.icon}  color='white'/>Feedback e avaliações</li>
+                        <li> <FontAwesomeIcon icon={faComment} size='1x'  className={styles.icon} color='white'/>Chat</li>
                     </ul>
                     <ul>
-                        <li>Sair</li>
+                        <li> <FontAwesomeIcon icon={faDoorOpen} size='1x'  className={styles.icon} color='white'/>Sair</li>
                     </ul>
                 </div>
             </div>
