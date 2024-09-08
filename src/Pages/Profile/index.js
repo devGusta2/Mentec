b5dcc2852397416513c4a4855b69fe075fb9a0b8
devGusta2 <gustavo.souza2979@gmail.com>
2024-09-08 11:09:39 -0300

    Profile page

---
+++ src/Pages/Profile/index.js
@@ -0,0 +1,17 @@
+
+
+import Header from "../../Components/Header/Header";
+import Menu from "../../Components/Menu/Menu";
+
+function Profile(){
+    return(
+        <div>
+            <Header />
+            
+            <Menu />
+        </div>
+    );
+}
+
+
+export default Profile;
