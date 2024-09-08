1741f5a32922d7651eee73718b2def86db39e37b
devGusta2 <gustavo.souza2979@gmail.com>
2024-09-08 11:40:38 -0300

    Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
-
+import {Link} from 'react-router-dom';
 
 function Header(){
     return(
@@ -19,7 +19,7 @@ function Header(){
                 </div>
                 <nav className={styles.navBar}>
                     <ul className={styles.ul}>
-                        <li>Início</li>
+                        <Link to='/Home' className={styles.link}><li>Inicio</li></Link>
                         <li>Mentorias</li>
                         <li>Contato</li>
                         <li>Ajuda</li>
@@ -31,7 +31,7 @@ function Header(){
                     <FontAwesomeIcon id="" icon={faUser} size='2x' color='white' className={styles.icon} />
                     <select className={styles.options}>
                         <option>Usuário</option>
-                        <option>Oerfil</option>
+                        <option>Perfil</option>
                         <option>Minhas mentorias</option>
                         <option>Configurações</option>
                         <option>Sair</option>
