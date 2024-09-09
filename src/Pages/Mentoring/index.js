0468054786bb6dd401a1ea4c0ca8d055e0f40285
devGusta2 <gustavo.souza2979@gmail.com>
2024-09-09 18:38:57 -0300

    Mentoring page layout

--- src/Pages/Mentoring/index.js
+++ src/Pages/Mentoring/index.js
@@ -3,6 +3,7 @@ import styles from './Mentoring.module.css';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import Carousel from '../../Components/Carousel/Carousel';
+import { Link } from 'react-router-dom';
 function Home(){
     return(
         <div className={styles.home}>
@@ -11,21 +12,25 @@ function Home(){
                 <div className={styles.content}>
                     <div className={styles.mainContent}>
                         <Carousel/>
+                        <div className={styles.filterBox}>
+                            <input 
+                            placeholder='Buscar mentorias'
+                            className={styles.searchBar}></input>
+                            <select>
+                                <option>Área</option>
+                            </select>
+                            <select>
+                                <option>Qntd Vagas</option>
+                            </select>
+                            <select>
+                                <option>Data</option>
+                            </select>
+                          
+                        </div>
                         <h1>Mentorias que podem te interessar</h1>
                         <div className={styles.carGrid}>
                             <div className={styles.card}>
-                                <div className={styles.cardRow}>
-                                    <div className={styles.infoBox}>
-                                        <h3>Introdução ao desenvolvimento de sistemas</h3>  
-                                    </div>
-                               
-                                </div>
-                                <div className={styles.cardRow}>
-                                    <p>Aprenda o básico do desenvolvimento 
-                                        web, incluindo HTML, CSS e JavaScript.
-                                         Ideal para iniciantes que querem...
-                                    </p>
-                                </div>
+                              
                             </div>
                             <div className={styles.card}></div>
                             <div className={styles.card}></div>
