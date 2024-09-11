2bed9f9478f465a1cc3b52a4aeeabf8c3f7dbbd9
devGusta2 <gustavo.souza2979@gmail.com>
2024-09-10 21:14:34 -0300

    Test page

---
+++ src/Pages/Tpage/index.js
@@ -0,0 +1,12 @@
+import Header from "../../Components/Header/Header"
+function Tpage(){
+    return(
+        <div>
+            <h3>Teste</h3>
+            <Header></Header>
+        </div>
+    )
+}
+
+
+export default Tpage;
