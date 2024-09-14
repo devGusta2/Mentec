a660a4a87ebdc62fc42ef23871ef05f63863078f
devGusta2 <gustavo.souza2979@gmail.com>
2024-09-14 14:37:19 -0300

    Contact page (not responsive)

--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -2,6 +2,7 @@ import styles from './Contact.module.css';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMap, faPhone, faEnvelope, faE } from '@fortawesome/free-solid-svg-icons';
 export default function Contact(){
     return(
         <div className={styles.contact}>
@@ -10,11 +11,45 @@ export default function Contact(){
                     <div className={styles.formBox}>
                         <div className={styles.info}>
                             <h1 id={styles.title1}>Contato</h1>
+                            <div className={styles.rowInfo}>
+                                <FontAwesomeIcon icon={faMap} size="2x"/>
+                                <p>
+                                    Rua Carlos de Carvalho 200, Ferraz de Vasconcelos, SP, 08545-120 ·  1 km
+                                </p>
+                            </div>
+                            <div className={styles.rowInfo}>
+                                <FontAwesomeIcon icon={faPhone} size="2x"/>
+                                <p>
+                                    (11)4679-6145
+                                </p>
+                            </div>
+                            <div className={styles.rowInfo}>
+                                <FontAwesomeIcon icon={faEnvelope} size="2x"/>
+                                <p>
+                                    Fatec.Ferraz@fatec.sp.gov.br
+                                    (arrumar depois)
+                                </p>
+                            </div>
                         </div>
                         <div className={styles.subFormBox}>
                             <h1 id={styles.title2}>Envie sua mensagem!</h1>
                             <form className={styles.form}>
+                                <input 
+                                placeholder="Nome:"
+                                className={styles.inptContact}/>
+                                <input 
+                                placeholder="E-mail:"
+                                className={styles.inptContact}/>
+                                <textarea 
+                                 placeholder='Mensagem:'
+                                 id={styles.msContact}>
 
+                                </textarea>
+                                <input 
+                               
+                                type='submit'
+                                name='enviar'
+                                id={styles.btnEnviar}/>
                             </form>
                         </div>
                     </div>
