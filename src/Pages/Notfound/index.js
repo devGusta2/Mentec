9bfcea1213106cb1e513f34d14effbaa92972ead
devGusta2 <gustavo.souza2979@gmail.com>
2024-09-14 14:50:02 -0300

    NotFound page styles

--- src/Pages/Notfound/index.js
+++ src/Pages/Notfound/index.js
@@ -1,8 +1,17 @@
 import Footer from "../../Components/Footer/Footer";
+import styles from './Notfound.module.css';
+import { Link } from "react-router-dom";
 function Notfound(){
     return(
-        <h1>Page not found!</h1>
+        <div className={styles.notFound}>
+            <h1 id={styles.title}>Page not found!</h1>
+            <Link to="Mentorships"id={styles.btnBack}>
+                <h2>Voltar</h2>
+            </Link>
+        </div>
     );
 }
 
 export default Notfound;
+
+
