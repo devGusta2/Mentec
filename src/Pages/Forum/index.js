dd7d4ac52036739e3f933e7df14ef2a371e7b764
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-03 13:14:28 -0300

    Forum

--- src/Pages/Forum/index.js
+++ src/Pages/Forum/index.js
@@ -1,5 +1,12 @@
-export default Forum(){
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+import style from './Forum.module.css';
+export default function Forum(){
     return(
-        <div></div>
+        <div>
+            <Header></Header>
+
+            <Footer></Footer>
+        </div>
     );
 }
