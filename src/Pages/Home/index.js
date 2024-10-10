23219b5f5202d5afdb5d81d18619649ff0e5d908
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-10 16:00:12 -0300

    Home page

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,9 @@
+import Footer from "../../Components/Footer/Footer"
+import Header from "../../Components/Header/Header"
+import Carousel from "../../Components/Carousel/Carousel"
+import styles from './Home.module.css'
+export default function Home(){
+    <div>
+        <Header />
+    </div>
+}
