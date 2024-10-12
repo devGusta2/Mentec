ffb95578e7aa117f9ae2106f31de1bc73f5c2bf6
= <=>
2024-10-12 12:32:08 -0300

    Home page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,15 @@
 import Footer from "../../Components/Footer/Footer"
 import Header from "../../Components/Header/Header"
 import Carousel from "../../Components/Carousel/Carousel"
-import styles from './Home.module.css'
+import style from './Home.module.css'
+
 export default function Home(){
-    <div>
-        <Header />
-    </div>
+    return(
+        <div >
+            <Header />
+            <Carousel />
+            oi
+            <Footer />
+        </div>
+    )
 }
