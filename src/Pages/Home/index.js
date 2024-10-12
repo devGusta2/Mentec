e71dba5948abda2258a9d2ecc20f15f027b950b9
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-12 16:19:01 -0300

    Home page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,10 @@
 import Footer from "../../Components/Footer/Footer"
 import Header from "../../Components/Header/Header"
 import Carousel from "../../Components/Carousel/Carousel"
-import style from './Home.module.css'
-
+import style from "./Home.module.css"
 export default function Home(){
     return(
-        <div >
+        <div className={style.container}>
             <Header />
             <Carousel />
             oi
