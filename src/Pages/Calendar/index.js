b458e03af2a5539ac7fcdcd0b90411d90d7ae6f1
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-13 17:24:35 -0300

    Calendar info

--- src/Pages/Calendar/index.js
+++ src/Pages/Calendar/index.js
@@ -1,11 +1,38 @@
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import styles from './Calendar.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar} from '@fortawesome/free-regular-svg-icons';
 export default function Calendar(){
     return(
         <div className={styles.container}>
             <Header/>
+            
             <div className={styles.grid}>
+                <div id={styles.timeTittle}>
+                    <p>Período</p>
+                </div>
+          
+                <div className={styles.day}>
+                    <p>26</p>
+                    <p>Segunda-feira</p>
+                </div>
+                <div className={styles.day}>
+                    <p>27</p>
+                    <p>Terça-feira</p>
+                </div>
+                <div className={styles.day}>
+                    <p>28</p>
+                    <p>Quarta-feira</p>
+                </div>
+                <div className={styles.day}>
+                    <p>29</p>
+                    <p>Quinta-feira</p>
+                </div>
+                <div className={styles.day}>
+                    <p>30</p>
+                    <p>Sexta-feira</p>
+                </div>
                 <div className={styles.card}></div>
                 <div className={styles.card}></div>
                 <div className={styles.card}></div>
@@ -25,6 +52,7 @@ export default function Calendar(){
                 <div className={styles.card}></div>
                 <div className={styles.card}></div>
             </div>
+ 
         </div>
     );
 }
