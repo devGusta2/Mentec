2dcdf5044d204c9cd7808c47c50c5232435fcc5f
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-13 17:11:05 -0300

    Calendar

--- src/Pages/Calendar/index.js
+++ src/Pages/Calendar/index.js
@@ -1,10 +1,30 @@
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
-
+import styles from './Calendar.module.css';
 export default function Calendar(){
     return(
-        <div className="container">
-
+        <div className={styles.container}>
+            <Header/>
+            <div className={styles.grid}>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+                <div className={styles.card}></div>
+            </div>
         </div>
     );
 }
