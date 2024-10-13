15e48c20a857807e559ee7f5b4473bb8598d16ad
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-13 16:56:41 -0300

    Calendar page

---
+++ src/Pages/Calendar/index.js
@@ -0,0 +1,10 @@
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+
+export default function Calendar(){
+    return(
+        <div className="container">
+
+        </div>
+    );
+}
