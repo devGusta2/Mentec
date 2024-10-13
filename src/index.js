9f4c5dc46d0db1fdbca8e1dbe45ef44814e050d0
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-13 17:11:56 -0300

    Calendar page path

--- src/index.js
+++ src/index.js
@@ -22,10 +22,15 @@ import Mentoring from './Pages/Mentoring';
 import Forum from './Pages/Forum';
 import Services from './Pages/Services';
 import Home from './Pages/Home';
+import Calendar from './Pages/Calendar';
 const router =  createBrowserRouter([
   {
     path:"*",
     element:<Notfound />
+  },
+  {
+    path: "Calendar",
+    element: <Calendar />
   },
   {
     path: "Home",
