a6e8799d41019384974884f396127c8baf36a706
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-14 13:34:39 -0300

    Modify

--- src/Pages/Tpage/index.js
+++ src/Pages/Tpage/index.js
@@ -1,9 +1,11 @@
 import Header from "../../Components/Header/Header"
+import Footer from "../../Components/Footer/Footer";
 function Tpage(){
     return(
         <div>
             <h3>Teste</h3>
-            <Header></Header>
+       
+            <Footer></Footer>
         </div>
     )
 }
