1e24e8c1b0ec5465f0b9cb85ebc09a85fe4214c4
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-15 18:24:11 -0300

    Forum page

--- src/Pages/Forum/index.js
+++ src/Pages/Forum/index.js
@@ -1,9 +1,9 @@
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
-import style from './Forum.module.css';
+import style from './Forum.module.css'
 export default function Forum(){
     return(
-        <div styles={style.container}>
+        <div className={style.container}>
             <Header></Header>
             
             <Footer></Footer>
