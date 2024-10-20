35c44f5d5441a9e9a869907704a58db71da67d89
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-19 22:01:04 -0300

    Forum page

--- src/Pages/Forum/index.js
+++ src/Pages/Forum/index.js
@@ -5,7 +5,26 @@ export default function Forum(){
     return(
         <div className={style.container}>
             <Header></Header>
-            
+                <div className={style.mainLayout}>
+                    <div className={style.menu}>
+                        <ul>
+                            <li>Discussões</li>
+                            <li>Buscar</li>
+                            <li>Últimos tópicos</li>
+                            <li>Melhores tópicos</li>
+                            <li>Salvos</li>
+                        </ul>
+                    </div>
+                    <main>
+                        <div className={style.title}>
+                            <h3>Conectando mentes, moldando futuros</h3>
+                            <p>
+                                Um espaço para compartilhar conhecimento, 
+                                trocar ideias e impulsionar a inovação tecnológica.
+                            </p>
+                        </div>
+                    </main>
+                </div>
             <Footer></Footer>
         </div>
     );
