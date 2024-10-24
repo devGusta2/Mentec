182eb6315b2f8a2ce48f5619def2cdbfeae8b42d
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-23 21:15:44 -0300

    Font awesome

--- src/Pages/Mentors/index.js
+++ src/Pages/Mentors/index.js
@@ -1,5 +1,10 @@
 import style from './Mentors.module.css';
 import Header from '../../Components/Header/Header';
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+
 export default function(){
     return(
         <div className={style.container}>
--- src/Pages/Tpage/index.js
+++ src/Pages/Tpage/index.js
@@ -1,11 +1,12 @@
 import Header from "../../Components/Header/Header"
 import Footer from "../../Components/Footer/Footer";
+
 function Tpage(){
     return(
         <div>
             <h3>Teste</h3>
        
-            <Footer></Footer>
+            <Header></Header>
         </div>
     )
 }
