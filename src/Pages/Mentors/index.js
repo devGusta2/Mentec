1fa2340c29c9e2612264f7a1338ac37593d9f8d0
devGusta2 <gustavo.souza2979@gmail.com>
2024-10-23 21:26:38 -0300

    Card mentors

--- src/Pages/Mentors/index.js
+++ src/Pages/Mentors/index.js
@@ -20,6 +20,27 @@ export default function(){
                 <div className={style.card}></div>
                 <div className={style.card}></div>
             </div>
+            <div className={style.cardMentor}>
+                <div className={style.colCard}>
+                    <h4>Nome:</h4>
+                    <p>
+                        Gelibolu da Silva Machado  
+                    </p>
+                    <h4>Sobre mim:</h4>
+                    <p>
+                    Com mais de 10 anos de experiência em desenvolvimento pessoal, Gelibolu da silva machado é um facilitador de transformação. Formado em Psicologia e Coaching, ele inspira indivíduos a superarem desafios e descobrirem seu verdadeiro potencial. Acredita que o autoconhecimento é a chave para o sucesso.
+                    bribes na DM.
+                    </p>
+                   
+                    
+                </div>
+                <div className={style.colCard}>
+
+                </div>
+                <div id={style.imgBox}>
+
+                </div>
+            </div>
         </div>
     );
 }
