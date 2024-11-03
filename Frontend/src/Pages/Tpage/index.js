32336e56b2a729497f5b2492529d9b8d019c6543
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-03 17:19:01 -0300

    Insert working

--- Frontend/src/Pages/Tpage/index.js
+++ Frontend/src/Pages/Tpage/index.js
@@ -17,8 +17,28 @@ function Tpage(){
     //     }
     //     buscaDados();
     // },[]);
+
+    const [email, setEmail] = useState([]);
+    const [nome, setNome] = useState("");
+    const [senha, setSenha] = useState("");
+    const [tel, setTel] = useState("");
+
+    const Cadastra = async (e) => { // Renomeando para Cadastra
+        e.preventDefault();
+        try {
+            const res = await axios.post("http://localhost/Api_mentec/Controller/user/cadastro.php", {
+                email,
+                nome,
+                senha,
+                tel,
+            });
+            console.log(res.data); // Opcional: Exibir resposta do backend
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return(
-        <div className="Container">
+        <div className={styles.container}>
             {/* <ul>
                             {dado.map(mi=>(
                             <li key={mi.id}>
@@ -47,19 +67,19 @@ function Tpage(){
                             </li>
                         ))}
                 </ul> */}
-            <section>
+            <section className={styles.section}>
                
-                <form>
+                <form onSubmit={Cadastra}>
                         <label>Nome:</label>
-                        <input type="text"/>
+                        <input type="text" value={nome} onChange={(e) => setNome(e.target.value)}/>
                         <label>Email:</label>
-                        <input type="text"/>
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                         <label>Senha:</label>
-                        <input type="text"/>
+                        <input type="password" value={senha}  onChange={(e) => setSenha(e.target.value)}/>
                         <label>Tel:</label>
-                        <input type="text"/>
+                        <input type="text" value={tel}  onChange={(e) => setTel(e.target.value)}/>
                         
-                        <button type="submit" onClick={enviar}>Enviar</button>
+                        <button type="submit" >Enviar</button>
                 </form>
             </section>
             <section>
