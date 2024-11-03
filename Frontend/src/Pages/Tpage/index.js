a4c07452161825ad3f4cc2cf2da946aac4dd1421
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-03 14:21:53 -0300

    Styles test page

--- Frontend/src/Pages/Tpage/index.js
+++ Frontend/src/Pages/Tpage/index.js
@@ -2,24 +2,24 @@ import Header from "../../Components/Header/Header"
 import Footer from "../../Components/Footer/Footer";
 import axios from 'axios';
 import { useEffect,useState  } from "react";
+import styles from './Tpage.module.css';
 function Tpage(){
-    const [dado, setDado] = useState([]);
-    useEffect(()=>{
-        const buscaDados = async () =>{
+    // const [dado, setDado] = useState([]);
+    // useEffect(()=>{
+    //     const buscaDados = async () =>{
           
-            try{
-                const res = await axios.get('http://localhost/Backend_Mentec/Controller/index.php');
-                setDado(res.data);
-            }catch(error){
-                console.log("Erro", error);
-            }
-        }
-        buscaDados();
-    },[]);
+    //         try{
+    //             const res = await axios.get('http://localhost/Backend_Mentec/Controller/index.php');
+    //             setDado(res.data);
+    //         }catch(error){
+    //             console.log("Erro", error);
+    //         }
+    //     }
+    //     buscaDados();
+    // },[]);
     return(
-        <div>
-            <h3>Teste</h3>
-                <ul>
+        <div className="Container">
+            {/* <ul>
                             {dado.map(mi=>(
                             <li key={mi.id}>
                                 {mi.id}
@@ -46,7 +46,25 @@ function Tpage(){
                                 {mi.num}
                             </li>
                         ))}
-                </ul>
+                </ul> */}
+            <section>
+               
+                <form>
+                        <label>Nome:</label>
+                        <input type="text"/>
+                        <label>Email:</label>
+                        <input type="text"/>
+                        <label>Senha:</label>
+                        <input type="text"/>
+                        <label>Tel:</label>
+                        <input type="text"/>
+                        
+                        <button type="submit" onClick={enviar}>Enviar</button>
+                </form>
+            </section>
+            <section>
+               
+            </section>
         </div>
     )
 }
