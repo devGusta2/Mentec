a077966e05287a40244eb35e18cb44ae46ca6ab4
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-09 14:20:23 -0300

    Create mentoring page teacher

---
+++ src/Pages/Teacher/CreateMentoring/index.js
@@ -0,0 +1,8 @@
+import styles from './CreateMentoring.module.css';
+export default function CreateMentoring(){
+    return(
+        <div className={styles.CreateMentoring}>
+
+        </div>
+    );
+}
