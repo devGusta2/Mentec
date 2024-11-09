7e93f18cc4d731c35e21252735b57713b1f1f15c
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-09 16:55:23 -0300

    Inputs create mentoring

--- src/Pages/Teacher/CreateMentoring/index.js
+++ src/Pages/Teacher/CreateMentoring/index.js
@@ -17,8 +17,50 @@ export default function CreateMentoring(){
             </div>
             <div className={styles.form_box}>
                 <form>
-                    
+                    <div className={styles.col_form}>
+                        <h4>Título</h4>
+                        <input 
+                        placeholder="Título da sua mentoria"
+                        className={styles.text_inpt}></input>
+                        <h4>Objetivo</h4>
+                        <input 
+                        placeholder="Descreva o objetivo de sua mentoria"
+                        className={styles.text_inpt}></input>
+                        <h4>Descrição</h4>
+                        <textarea 
+                        placeholder="Descreva sua mentoria"></textarea>
+                    </div>
+                    <div className={styles.col_form}>
+                        <h4>Duração</h4>
+                            <input 
+                            type='number'
+                            min="12"
+                            max="200"
+                            placeholder="Carga horária estimada"
+                            className={styles.text_inpt}></input>
+                        <h4>Período</h4>
+                            <select className={styles.text_inpt}>
+                                <option>Manhã</option>
+                                <option>Tarde</option>
+                                <option>Noite</option>
+                            </select>
+                        <h4>Modalidade</h4>
+                            <select className={styles.text_inpt}>
+                                <option>Presencial</option>
+                                <option>Ead</option>
+                                <option>Híbrido</option>
+                            </select>
+                       
+                        <h4>Público alvo</h4>
+                        <input 
+                        placeholder="Quem você deseja alcançar"
+                        className={styles.text_inpt}></input>
+                    </div>
+                      <div className={styles.next_btn}>
+                            <h3>Próximo</h3>
+                     </div>
                 </form>
+               
             </div>
         </div>
     );
