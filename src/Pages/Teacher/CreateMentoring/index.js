a8cdda833359d76495423786471b257e63567d1e
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-10 14:10:00 -0300

    Btn next

--- src/Pages/Teacher/CreateMentoring/index.js
+++ src/Pages/Teacher/CreateMentoring/index.js
@@ -8,60 +8,63 @@ export default function CreateMentoring(){
                 <div className={styles.titles_art}>
                     <div className={styles.titles_box}>
                         <h3>Voltar</h3>
-                        <p>Passo 1 de 4</p>
+                        <p>Passo 1 de 3</p>
                         <h1>Compartilhe seu conhecimento</h1>
                         <p>Publique sua mentoria e inspire novos talentos!</p>
                     </div>
                     <img src={hat} id={styles.hat}></img>
                 </div>
             </div>
+
+
             <div className={styles.form_box}>
                 <form>
                     <div className={styles.col_form}>
-                        <h4>Título</h4>
+                        <h4>* Título</h4>
                         <input 
                         required
                         placeholder="Título da sua mentoria"
                         className={styles.text_inpt}></input>
-                        <h4>Objetivo</h4>
+                        <h4>* Objetivo</h4>
                         <input 
                         placeholder="Descreva o objetivo de sua mentoria"
                         className={styles.text_inpt}></input>
-                        <h4>Descrição</h4>
+                        <h4>* Descrição</h4>
                         <textarea 
                         placeholder="Descreva sua mentoria"></textarea>
                     </div>
                     <div className={styles.col_form}>
-                        <h4>Duração</h4>
+                        <h4>* Duração</h4>
                             <input 
                             type='number'
                             min="5"
                             max="200"
                             placeholder="Carga horária estimada"
                             className={styles.text_inpt}></input>
-                        <h4>Período</h4>
+                        <h4>* Período</h4>
                             <select className={styles.text_inpt}>
                                 <option>Manhã</option>
                                 <option>Tarde</option>
                                 <option>Noite</option>
                             </select>
-                        <h4>Modalidade</h4>
+                        <h4>* Modalidade</h4>
                             <select className={styles.text_inpt}>
                                 <option>Presencial</option>
                                 <option>Ead</option>
                                 <option>Híbrido</option>
                             </select>
                        
-                        <h4>Público alvo</h4>
+                        <h4>* Público alvo</h4>
                         <input 
                         placeholder="Quem você deseja alcançar"
                         className={styles.text_inpt}></input>
+                            <div className={styles.next_btn}>
+                                <h4>Próximo</h4>
+                            </div>
                     </div>
-                      <div className={styles.next_btn}>
-                            <h3>Próximo</h3>
-                     </div>
+              
                 </form>
-               
+           
             </div>
         </div>
     );
