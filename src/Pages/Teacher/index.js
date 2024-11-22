baaf5aa2ab31e1cd7d99e9f8907349a9f398fc9a
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-21 22:53:16 -0300

    Menu teaher's page

--- src/Pages/Teacher/index.js
+++ src/Pages/Teacher/index.js
@@ -1,9 +1,21 @@
 import {Link} from 'react-router-dom';
+import styles from './Teacher.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faPersonChalkboard} from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 export default function Teacher(){
     return(
-        <div className="">
-            <h3>Página do professor</h3>
-            <Link to='CreateMentoring'><h3>Criar </h3></Link>
+        <div className={styles.Teacher}>
+            <div className={styles.teacher_menu}>
+                <nav className={styles.nav_menu}>
+                    <ul>
+                    <li><FontAwesomeIcon className={styles.icon} icon={faUser} size='2x'/><Link to='createMentoring'>Perfil</Link></li>
+                        <li><FontAwesomeIcon className={styles.icon} icon={faPlus} size='2x'/><Link to='createMentoring'>Nova mentoria</Link></li>
+                        <li><FontAwesomeIcon className={styles.icon} icon={faPersonChalkboard} size='2x'/><Link to='createMentoring'>Minhas mentorias</Link></li>
+                        
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 }
