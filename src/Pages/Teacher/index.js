64f450ef7d53aa51dc6c252fec8e5906ce88fa6d
devGusta2 <gustavo.souza2979@gmail.com>
2024-11-21 23:03:47 -0300

    Menu component

--- src/Pages/Teacher/index.js
+++ src/Pages/Teacher/index.js
@@ -1,21 +1,10 @@
-import {Link} from 'react-router-dom';
-import styles from './Teacher.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faPersonChalkboard} from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
+
+import MenuTeacher from '../../Components/Menu_Teacher/Menu_Taeacher';
+import styles from './Teacher.module.css';
 export default function Teacher(){
     return(
-        <div className={styles.Teacher}>
-            <div className={styles.teacher_menu}>
-                <nav className={styles.nav_menu}>
-                    <ul>
-                    <li><FontAwesomeIcon className={styles.icon} icon={faUser} size='2x'/><Link to='createMentoring'>Perfil</Link></li>
-                        <li><FontAwesomeIcon className={styles.icon} icon={faPlus} size='2x'/><Link to='createMentoring'>Nova mentoria</Link></li>
-                        <li><FontAwesomeIcon className={styles.icon} icon={faPersonChalkboard} size='2x'/><Link to='createMentoring'>Minhas mentorias</Link></li>
-                        
-                    </ul>
-                </nav>
-            </div>
+        <div className=''>
+            <MenuTeacher />
         </div>
     );
 }
