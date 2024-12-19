7567a39a7defc1a204d4cc9b08a56f871a2640d7
devGusta2 <gustavo.souza2979@gmail.com>
2024-12-18 23:43:26 -0300

    New login layout

--- src/Pages/login/index.js
+++ src/Pages/login/index.js
@@ -6,51 +6,24 @@ import Accessibility from '../../Components/Acessibility/Accessibility';
 function Login(){
     return(
         <div className={styles.main}>
-           <div className={styles.cadBox}>
-                <div className={styles.logoBox}>
-                    <h1  className={styles.title}>Mentec</h1>
-                    <h3  className={styles.sBtitle}>Conectando mentes, moldando futuros</h3>
-                </div>
-                <img 
-                draggable="false"
-                src={book} className={styles.imgBook}/>
-                <Link to='/cadastro'className={styles.btnCad}>
+            <div className={styles.header}>
+                <h2>Mentec</h2>
+            </div>
+            <div className={styles.formAndArt}>
+                <form className={styles.formLogin}>
+                    <p>Fazer login</p>
+                    <input className={styles.inpt} placeholder='E-mail'/>
+                    <input className={styles.inpt} placeholder='Senha'/>
+                    <p>Esqueceu sua senha?</p>
                     
-                    <h1>Cadastrar</h1>
-                </Link>
-           </div>
-           <div className={styles.formBox}>
-            
-                <div className={styles.form}>
-                    
-                    <form className={styles.formLogin}>
-                        <div className={styles.titleBox}>
-                            <h3>Fazer login</h3>
-                        </div>
-                        <input 
-                        placeholder='E-mail'
-                        className={styles.inptForm}/>
-                        <input 
-                        placeholder='Senha'
-                        className={styles.inptForm}/>
+                    <div id={styles.formRow}>
+                        <input type='checkbox' id={styles.check}/>
+                        <p>Lembre-se de mim</p>
+                        <button id={styles.btnLogin} type='submit'>Fazer login</button>
+                    </div>
+                </form>
+                <div className={styles.art}>
 
-                        <button className={styles.btn}>
-                            <h3>Fazer login</h3>
-                        </button>
-                        <div className={styles.or}>
-                            <hr className={styles.line}/>
-                            <h3>ou</h3>
-                            <hr className={styles.line}/>
-                        </div>
-                        <button className={styles.btnms}>
-                            <img src={msIco} className={styles.msico}/>
-                            <h3>Continue com a microsoft</h3>
-                        </button>
-                        <div className={styles.cadResp}>
-                            <h4>Não tem uma conta ainda?</h4>
-                            <Link to='/Signup' className={styles.linkCad}>Cadastre-se</Link>
-                        </div>
-                    </form>
                 </div>
             </div>
             <Accessibility />
