5d277434465f0f81aa8a0164921f1e767329c9b9
devGusta2 <gustavo.souza2979@gmail.com>
2024-12-21 02:23:55 -0300

    Page recovery text

--- src/Pages/Recovery/index.js
+++ src/Pages/Recovery/index.js
@@ -2,6 +2,7 @@ export default function Recovery(){
     return(
         <div>
             <h3>Recupere sua conta!</h3>
+            <p>Por favor, digite o email da conta que você quer recuperar.</p>
         </div>
     );
 }
