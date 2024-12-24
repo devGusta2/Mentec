1fddde9453cb37591d9b3a0490a7677b830b50b0
devGusta2 <gustavo.souza2979@gmail.com>
2024-12-24 17:18:48 -0300

    Header recovery page

--- src/Pages/Recovery/index.js
+++ src/Pages/Recovery/index.js
@@ -1,6 +1,14 @@
-export default function Recovery(){
-    return(
-        <div>
+import styles from './Recovery.module.css';
+export default function Recovery() {
+    return (
+
+        <div className={styles.main}>
+            <div className={styles.header}>
+                <h2>Mentec</h2>
+                <button id={styles.cadBtn}>
+                    <h4>Criar uma conta no mentec</h4>
+                </button>
+            </div>
             <h3>Recupere sua conta!</h3>
             <p>Por favor, digite o email da conta que você quer recuperar.</p>
         </div>
