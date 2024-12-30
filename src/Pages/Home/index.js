ea29ad12ab1d29c979198b74e621499a485995d4
devGusta2 <gustavo.souza2979@gmail.com>
2024-12-30 15:56:49 -0300

    News layout

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,15 +3,32 @@ import Header from "../../Components/Header/Header"
 import Carousel from "../../Components/Carousel/Carousel"
 import styles from "./Home.module.css"
 import Accessibility from "../../Components/Acessibility/Accessibility"
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <div className={styles.container}>
             <Header />
-            <Carousel />
-           
+            {/* <Carousel /> */}
+
             <div className={styles.news_container}>
-                <h2>Notícias</h2>
-                
+         
+                <div className={styles.colNews}>
+                    <div className={styles.newsBox}>
+                        <img src='https://thumbs.dreamstime.com/b/programmierer-der-mit-programmcode-arbeitet-131979164.jpg' className={styles.img}>
+
+                        </img>
+                        <h3>Conheça as equipes ganhadoras do Interfatecs</h3>
+                        <p>
+                            Fique por dentro também das equipes da sua fatec 
+                            participaram do interfatecs
+                        </p>
+                    </div>
+                    <div className={styles.newsBox}>
+
+                    </div>
+                </div>
+                <div className={styles.colNews}>
+
+                </div>
             </div>
             <Accessibility />
             <Footer />
