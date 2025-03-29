b76d01dc03ead62242cba705176c35d5fea3051a
devGusta2 <gustavo.souza2979@gmail.com>
2025-03-28 21:31:12 -0300

    Chat estático, faltam: deixar responsivo, alguns efeitos visuais, integração com back-end, botoes de anexar arquivos e enviar mensagem, etc.

--- src/Pages/Chat/index.js
+++ src/Pages/Chat/index.js
@@ -7,6 +7,36 @@ export default function Chat(){
                 <div id={styles.titlePageChat}>
                     <p>Chat</p>
                 </div>
+                <div id={styles.searchBarBox}>
+                    <input placeholder='Pesquisar'/>
+                </div>
+                <div id={styles.conversations}>
+                    <div className={styles.conversations_box}>
+                        <div className={styles.imgBox}>
+                        
+                        </div>
+                        <div className={styles.conversationsInfo}>
+                            <div className={styles.nameMessage}></div>
+                            <div className={styles.additionalInfo}></div>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+            <div id={styles.contentMessages}>
+                <div id={styles.upInfoBox}></div>
+                <div className={styles.messagesBox}>
+                    <div className={styles.senderMessage}>Bom dia</div>
+                    <div className={styles.receiverMessage}>Boa tarde</div>
+                </div>
+                <div id={styles.messageInputBox}>
+                    <input placeholder='DIgite sua mensagem'>
+
+                    </input>
+                    <div id={styles.optionalFunc}>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     );
