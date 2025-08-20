7835dfbdf287c789d1c5981ee55171cbd6dc15d6
devGusta2 <gustavo.souza2979@gmail.com>
2025-08-20 00:08:35 -0300

    Menu lateral admin completo

--- src/Pages/Admin/Components/Menu/Menu.js
+++ src/Pages/Admin/Components/Menu/Menu.js
@@ -1,7 +1,9 @@
 import styles from './Menu.module.css';
 import { Link } from 'react-router-dom';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon, faChart } from '@fortawesome/react-fontawesome';
+import { faUser, faMessage } from '@fortawesome/free-regular-svg-icons';
+import { faChartSimple, faChartBar, faBell, faGraduationCap,faDoorOpen } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export default function MenuAdm() {
     return (
         <div className={styles.menu}>
@@ -16,36 +18,47 @@ export default function MenuAdm() {
                 <ul>
                     <li>
                         <Link className={styles.link}>
+                            <FontAwesomeIcon icon={faChartSimple} />
                             <p>Dashboard</p>
                         </Link>
                     </li>
-                     <li>
+                    <li>
                         <Link className={styles.link}>
+                            <FontAwesomeIcon icon={faChartBar} />
                             <p>Análise</p>
                         </Link>
                     </li>
-                     <li>
+                    <li>
                         <Link className={styles.link}>
+                            <FontAwesomeIcon icon={faBell} />
                             <p>Denúncias</p>
                         </Link>
                     </li>
-                     <li>
+                    <li>
                         <Link className={styles.link}>
-                            <FontAwesomeIcon icon={faUser}/>
+                            <FontAwesomeIcon icon={faUser} />
                             <p>Usuários</p>
                         </Link>
                     </li>
-                     <li>
+                    <li>
                         <Link className={styles.link}>
+                            <FontAwesomeIcon icon={faMessage} />
                             <p>Mensagens</p>
                         </Link>
                     </li>
-                     <li>
+                    <li>
                         <Link className={styles.link}>
+                            <FontAwesomeIcon icon={faGraduationCap} />
                             <p>Mentorias</p>
                         </Link>
                     </li>
                 </ul>
+            </div>
+            <div className={styles.ult_btn}>
+                <Link className={styles.link}>
+                    <FontAwesomeIcon icon={faDoorOpen} />
+                    <p>Sair</p>
+                </Link>
             </div>
         </div>
     );
