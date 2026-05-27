32dd2b26ced876458d82396e9c4b10d65a389ddd
Larissa Matsuda <larissamatsuda10@gmail.com>
2026-05-26 21:27:14 -0300

    Update index.js

--- src/Pages/login/index.js
+++ src/Pages/login/index.js
@@ -71,6 +71,8 @@ function Login() {
                 window.location.href = "/mentor/home";
             } else if (response.data.role === "MONITOR") {
                 window.location.href = "/monitor/home";
+            } else if (response.data.role === "COORDENADOR") {
+                window.location.href = "/monitor/home";
             } else {
                 window.location.href = "/";
             }
